bb13c92f6933a9a388a633f5705f8256892c489e
dutraneto <dudutraneto@gmail.com>
2019-01-24 22:32:08 -0500

    add preloader

--- js/select-img.js
+++ js/select-img.js
@@ -1,34 +1,43 @@
-var a = document.getElementById("a");
-var img = document.getElementById("img");
-var fig = document.getElementById("fig");
+var a = document.getElementById('a');
+var img = document.getElementById('img');
+var fig = document.getElementById('fig');
 
-a.addEventListener('mouseover', (event) => {
-  var event = event.target;
-  event = showImg();
+a.addEventListener('mouseover', event => {
+    var event = event.target;
+    event = showImg();
 });
 
-a.addEventListener('mouseout', (event) => {
-  var event = event.target;
-  event = hideImg();
-  reset();
-  
+a.addEventListener('mouseout', event => {
+    var event = event.target;
+    event = hideImg();
+    reset();
 });
 
 function showImg() {
-  img.style.filter = "brightness(100%) blur(0)";
-  img.style.transform = "scale(1)";
-  fig.style.top = "100%";
-  fig.style.transform = "translateX(-50%)";
+    img.style.filter = 'brightness(100%) blur(0)';
+    img.style.transform = 'scale(1)';
+    fig.style.top = '100%';
+    fig.style.transform = 'translateX(-50%)';
 }
 
-function hideImg() { 
-  img.style.filter = "brightness(70%) blur(8px)";
-  img.style.transform = "scale(1.5)";
-  fig.style.top = "50%";
-  fig.style.transform = "translate(-50%, -50%)";
+function hideImg() {
+    img.style.filter = 'brightness(70%) blur(8px)';
+    img.style.transform = 'scale(1.5)';
+    fig.style.top = '50%';
+    fig.style.transform = 'translate(-50%, -50%)';
 }
 
 function reset() {
-  img.removeAttribute('style');
-  fig.removeAttribute('style');
+    img.removeAttribute('style');
+    fig.removeAttribute('style');
 }
+
+// ///////////
+
+// pre-loader
+
+/////////////
+let loader = document.getElementById('loader');
+window.addEventListener('load', function() {
+    loader.style.display = 'none';
+});
